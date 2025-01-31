947dcfa8c6d1954b108dfd3f44c9dbab7749e5c6
Joona <joona.kivela@student.lab.fi>
2025-01-31 19:38:16 +0200

    Calculator basic functions and ui implemented

--- app/src/main/java/fi/joonakivela/simplecalculatorapp/MainActivity.kt
+++ app/src/main/java/fi/joonakivela/simplecalculatorapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package fi.joonakivela.simplecalculatorapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editOper1: EditText
+    private lateinit var editOper2: EditText
+    private lateinit var textResult: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,39 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        editOper1 = findViewById(R.id.editTextOperand1)
+        editOper2 = findViewById(R.id.editTextOperand2)
+        textResult = findViewById(R.id.textViewResult)
+    }
+
+    fun doCalc(view: View) {
+        val oper1 = editOper1.text.toString().toDoubleOrNull()
+        val oper2 = editOper2.text.toString().toDoubleOrNull()
+        if (oper1 == null || oper2 == null) {
+            textResult.text= getString(R.string.please_enter_number_values)
+            return
+        }
+        var result: Double = 0.0
+        when (view.id) {
+            R.id.buttonPlus -> result = oper1 + oper2
+            R.id.buttonSub -> result = oper1 - oper2
+            R.id.buttonMul -> result = oper1 * oper2
+            R.id.buttonDiv -> {
+                if (oper2 == 0.0) {
+                    textResult.text = getString(R.string.division_by_zero)
+                    return
+                }
+                result = oper1 / oper2
+            }
+            R.id.buttonMod -> {
+                if (oper2 == 0.0) {
+                    textResult.text = getString(R.string.division_by_zero)
+                    return
+                }
+                result = oper1 % oper2
+            }
+            R.id.buttonPow -> result = Math.pow(oper1, oper2)
+        }
+        textResult.text = String.format(Locale("Finnish", "FI"),"%.2f", result)
     }
 }
